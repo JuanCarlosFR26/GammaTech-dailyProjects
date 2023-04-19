8789984004a3e27d4728cd790f81d814780f61ba
unknown <jcf.daw@gmail.com>
2023-04-19 17:41:33 +0200

    adding second and third generations

---
+++ Pokedex/src/home.js
@@ -0,0 +1,16 @@
+const firstGen = document.getElementById('firstGen');
+const secondGen = document.getElementById('secondGen');
+const thirdGen = document.getElementById('terceraGen');
+
+firstGen.addEventListener('click', () => {
+    window.location.href = '../pages/primeraGen.html'
+})
+
+secondGen.addEventListener('click', () => {
+    window.location.href = '../pages/segundaGen.html'
+})
+
+thirdGen.addEventListener('click', () => {
+    window.location.href = '../pages/terceraGen.html'
+})
+
